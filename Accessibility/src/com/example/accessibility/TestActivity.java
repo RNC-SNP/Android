f563883ca45191b38ad0ad2d8564373148639d21
RincLiu <i@RincLiu.com>
2015-07-24 10:51:02 +0800

    Accessibility Service.

---
+++ Accessibility/src/com/example/accessibility/TestActivity.java
@@ -0,0 +1,17 @@
+package com.example.accessibility;
+
+import com.example.accessibility.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
